1992dc04f77277c7abd426365d618d82e350b006
Christopher Hiller <boneskull@boneskull.com>
2026-04-15 15:57:27 -0700

    fix(module-source): fix SourceMapHook & other types
    This fixes the type of `SourceMapHook` and the implementation to align with reality.
    
    - Introduces some `.ts` sources for types; fixes `tsconfig.json`
    - Fixes exports of said types
    - Fixes shim types
    - Adds a `typedoc.json` for proper entry points

--- packages/module-source/index.js
+++ packages/module-source/index.js
@@ -1,1 +1,9 @@
+// eslint-disable-next-line import/export
+export * from './src/external.types.js';
+
 export { ModuleSource } from './src/module-source.js';
+export { CjsModuleSource } from './src/cjs-module-source.js';
+export {
+  createModuleSourcePasses,
+  createCjsModuleSourcePasses,
+} from './src/visitor-passes.js';
